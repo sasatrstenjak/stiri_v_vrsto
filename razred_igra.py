d68bf26be09657858d8251f3337aebdd06130452
Andrej Bauer <Andrej.Bauer@andrej.com>
2017-05-05 09:08:43 +0200

    Spremembe iz govorilne ure 5. 5. 2017
    * spremeni predstavitev poteze: samo stolpec, namesto (stolpec, vrstica)
    * zmagovalni okvir je zdaj označen s `TAG_STIRICA`
    * dodali `narisi_figuro`, da ne računamo koordinat dvakrat
    * manjši lepotni popravki

--- razred_igra.py
+++ razred_igra.py
@@ -27,9 +27,9 @@ class Igra():
             i +=1
 
         self.na_potezi = MODRI #začne modri igralec
-        
+
         self.zgodovina = []
-        
+
         self.stirice = []
         #doda vse možne zmage po vseh stolpcih
         for i in range(7):
@@ -47,7 +47,7 @@ class Igra():
                 self.stirice.append([(i,j),(i+1,j+1),(i+2,j+2),(i+3,j+3)])
                 self.stirice.append([(i,j+3),(i+1,j+2),(i+2,j+1),(i+3,j)])
 
-        
+
     def shrani_pozicijo(self):
         """Shrani trenutno pozicijo, da se bomo lahko kasneje vrnili vanjo
            z metodo razveljavi."""
@@ -61,52 +61,45 @@ class Igra():
         k.na_potezi = self.na_potezi
         return k
 
-    
+
     def razveljavi(self):
         """Razveljavi potezo in se vrni v prejšnje stanje."""
         (self.stolpci, self.na_potezi) = self.zgodovina.pop()
 
     def veljavne_poteze(self):
         """Vrni seznam veljavnih potez."""
-        veljavne_poteze = []
-        for i in range(7):
-            for j in range(5, -1, -1):
-                if self.stolpci[i][j] == 0:
-                    veljavne_poteze.append((i, j))
-                    break
-        # print("Veljavne poteze: {}".format(veljavne_poteze))
-        return veljavne_poteze
+        return [i for i in range(7) if self.stolpci[i][0] == 0]
 
 
-    def povleci_potezo(self, p):
-        """Povleci potezo p, ne naredi nič, če je neveljavna.
-           Vrne stanje_igre() po potezi ali None, ce je poteza neveljavna."""
+    def povleci_potezo(self, i):
+        """Povleci potezo v stolpcu i. Ne naredi nič, če je i neveljaven, ali je ta stolpec že poln.
+           Če je poteza neveljavna, vrne None. Sicer vrne par (j, stanje_igre()), kjer
+           je j vrstica, v katero smo odigrali žeton."""
 
-        (i, j) = p
-        if self.stolpci[i][0] != 0 or (self.na_potezi == None):
-             #neveljavna poteza
-             return None
-        if i < 0 or i > 6 or j < 0: #to zagotovi, da je klik izven igralnega polja neveljaven
+        if i < 0 or i > 6: #to zagotovi, da je klik izven igralnega polja neveljaven
+            return None
+        elif self.stolpci[i][0] != 0 or (self.na_potezi == None):
+            #neveljavna poteza
             return None
         else:
             self.shrani_pozicijo()
             j = 5
-            while j>=0: #v matriko self.stolpci se zapiše, da je polje zasedeno
+            while j >= 0: #v matriko self.stolpci se zapiše, da je polje zasedeno
                 if self.stolpci[i][j] == 0:
                     self.stolpci[i][j] = self.na_potezi
                     break
-                j-=1
+                j -= 1
+            assert (j >= 0)
             (zmagovalec, stirica) = self.stanje_igre()
             if zmagovalec == NI_KONEC:
                 # Igre ni konec, zdaj je na potezi nasprotnik
                 self.na_potezi = nasprotnik(self.na_potezi)
-                pass
             else:
                 # Igre je konec
                 self.na_potezi = None
-            return (zmagovalec, stirica)
+            return (j, (zmagovalec, stirica))
 
-    def stanje_igre(self): 
+    def stanje_igre(self):
         """Ugotovi, kakšno je trenutno stanje igre. Vrne:
            - (MODRI, stirica), če je igre konec in je zmagal MODRI z dano zmagovalno stirico
            - (RDECI, stirica), če je igre konec in je zmagal RDECI z dano zmagovalno stirico
@@ -119,14 +112,13 @@ class Igra():
             if p != 0 and p == self.stolpci[i2][j2] == self.stolpci[i3][j3] == self.stolpci[i4][j4]:
                 # Našli smo zmagovalno stirico
                 return (p, (stirica[0], stirica[1], stirica[2], stirica[3]))
-    
+
         # Ni zmagovalca, ali je igre konec?
         for i in range(7):
             for j in range(6):
                 if self.stolpci[i][j] == 0:
                     # Našli smo prazno polje, igre ni konec
                     return (NI_KONEC, None)
-                        
+
         # Vsa polja so polna, rezultat je neodločen
         return (NEODLOCENO, None)
-             
